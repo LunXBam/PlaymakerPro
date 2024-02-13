748b2101f4115354c4a242dbe1bb09026143276e
adilnabi <anabi@lakeheadu.ca>
2024-02-13 17:02:50 -0500

    Created Player Activity and basic Player xml

---
+++ app/src/main/java/com/example/coaching_app/PlayerActivity.kt
@@ -0,0 +1,10 @@
+package com.example.coaching_app
+
+import androidx.appcompat.app.AppCompatActivity
+
+
+
+class PlayerActivity : AppCompatActivity()
+{
+//    private lateinit var binding: PlayerActivityBinding
+}
