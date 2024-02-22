dda5f2def92cf8f88ee2fe0f6389b4f165746343
LunXBam <xcflores@lakeheadu.ca>
2024-02-22 11:02:14 -0500

    game history activity and kotlin class created

---
+++ app/src/main/java/com/example/coaching_app/GameHistoryActivity.kt
@@ -0,0 +1,11 @@
+package com.example.coaching_app
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class GameHistoryActivity : AppCompatActivity () {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.game_history)
+    }
+}
