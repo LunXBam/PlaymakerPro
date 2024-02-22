7825f26d4443dbdcf96af53d187c08a8455e2da6
LunXBam <xcflores@lakeheadu.ca>
2024-02-22 13:47:42 -0500

    created onCreate function and fixed xml of login

--- app/src/main/java/com/example/coaching_app/Login.kt
+++ app/src/main/java/com/example/coaching_app/Login.kt
@@ -1,4 +1,11 @@
 package com.example.coaching_app
 
-class login {
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Login : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
 }
