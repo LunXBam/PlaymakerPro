700e624a71fcd7c5bbf9ca02193fefd0f1b64dba
LunXBam <xcflores@lakeheadu.ca>
2024-02-22 15:24:26 -0500

    added basic view activity to allow emulator to launch

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -74,6 +74,8 @@ dependencies {
     //Required for RecyclerView
     implementation("androidx.recyclerview:recyclerview:1.3.2")
     implementation("com.google.firebase:firebase-firestore:24.10.2")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
+    implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
