285cc9b6ba6874f193ecb89c8962a83c4779a587
LunXBam <xcflores@lakeheadu.ca>
2024-02-23 18:57:32 -0500

    created an empty constructor for GameHistory class obj

--- app/src/main/java/com/example/coaching_app/GameHistory.kt
+++ app/src/main/java/com/example/coaching_app/GameHistory.kt
@@ -1,13 +1,17 @@
 package com.example.coaching_app
 
-data class GameHistory (
+class GameHistory (
     var teamID: String? = null,
     var gameDate: String? = null,
     var opponent: String? = null,
     var result: String? = null,
     var ourScore: String? = null,
-    var oppScore: String? = null,
+    var opponentScore: String? = null,
     var venue: String? = null,
     var weather: String? = null,
     var gameID: String
-)
+){
+
+
+    constructor():this(null,null,null,null,null,null,null,null,"")
+}
