f5b90dddd02206735897ae0a9e5915e93ca3bfbe
LunXBam <xcflores@lakeheadu.ca>
2024-02-24 15:14:17 -0500

    created/completed functionality of creating a new game history

---
+++ app/src/main/java/com/example/coaching_app/CreateGameHistoryActivity.kt
@@ -0,0 +1,76 @@
+package com.example.coaching_app
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.example.coaching_app.databinding.ActivityCreateGameHistoryBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.ktx.Firebase
+
+
+class CreateGameHistoryActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityCreateGameHistoryBinding
+    private lateinit var auth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_game_history)
+        binding = ActivityCreateGameHistoryBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.createGameHist.setOnClickListener{
+            val oppName = binding.opponentEditText.text.toString().trim()
+            val oppScore = binding.oppScoreEditText.text.toString().trim()
+            val ourScore = binding.ourScoreEditText.text.toString().trim()
+            val result = binding.resultEditText.text.toString().trim()
+            val gameDate = binding.gameDateEditText.text.toString().trim()
+            val venue = binding.venueEditText.text.toString().trim()
+            val weather = binding.weatherEditText.text.toString().trim()
+
+            if(oppName.isNotEmpty() && oppScore.isNotEmpty() && ourScore.isNotEmpty()
+                && result.isNotEmpty() && gameDate.isNotEmpty() && venue.isNotEmpty() && weather.isNotEmpty()){
+
+                // initalized db
+                val db = FirebaseFirestore.getInstance().collection("game_history")
+
+                val id = db.document().id
+
+                // create the new game history
+                val gameHist = GameHistory(id,gameDate,oppName,result,ourScore,oppScore,venue,weather,"1" )
+
+                db.document(id).set(gameHist)
+                    .addOnSuccessListener {
+                        Toast.makeText(this, "Game History Successfully Created",
+                            Toast.LENGTH_LONG).show() }
+                    .addOnFailureListener{
+                        it.localizedMessage?.let { it1 -> Log.w("DB issue", it1) }
+                    }
+
+                val intent = Intent(this, GameHistoryActivity::class.java)
+                startActivity(intent)
+            }
+
+            else{
+                Toast.makeText(this, "All fields must be filled in!!!!",
+                    Toast.LENGTH_LONG).show()
+            }
+
+
+        }
+
+
+        binding.goBack.setOnClickListener{
+            val intent = Intent(this, GameHistoryActivity::class.java)
+            startActivity(intent)
+        }
+
+
+
+    }
+
+
+}
