cceab31dc233207551b31f4dde26fa5b0113ff43
LunXBam <xcflores@lakeheadu.ca>
2024-03-24 15:39:19 -0400

    scheduler works but only for selected a date

---
+++ app/src/main/java/com/example/coaching_app/SchedulerActivity.kt
@@ -0,0 +1,48 @@
+package com.example.coaching_app
+
+import android.app.DatePickerDialog
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import com.example.coaching_app.databinding.ActivitySchedulerBinding
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Locale
+
+class SchedulerActivity : AppCompatActivity() {
+
+    private lateinit var tvDate : TextView
+    private lateinit var btnShowDatePicker : Button
+    private val calendar = Calendar.getInstance()
+    private lateinit var binding: ActivitySchedulerBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySchedulerBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        tvDate = findViewById<TextView>(R.id.tvSelectDate)
+        btnShowDatePicker = findViewById<Button>(R.id.btnShowDatePicker)
+
+        btnShowDatePicker.setOnClickListener{
+            showDatePicker()
+        }
+    }
+
+    private fun showDatePicker(){
+        val datePickerDialog = DatePickerDialog(this,{DatePicker, year : Int, monthOfYear : Int, dayOfMonth : Int ->
+            val selectedDate = Calendar.getInstance()
+            selectedDate.set(year, monthOfYear, dayOfMonth)
+            val dateFormat = SimpleDateFormat("dd/MM/yyyy", Locale.getDefault())
+            val formattedDate = dateFormat.format(selectedDate.time)
+            tvDate.text = "Selected Date: " + formattedDate
+        },
+            calendar.get(Calendar.YEAR),
+            calendar.get(Calendar.MONTH),
+            calendar.get(Calendar.DAY_OF_MONTH)
+        )
+
+        datePickerDialog.show()
+    }
+}
